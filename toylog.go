4db4e041423457781c25c31d3bec37d715e17567
ctacampado <ct.acampado@gmail.com>
2020-01-02 10:13:55 +0800

    fixed tests

---
+++ toylog.go
@@ -0,0 +1,121 @@
+// Created on Sat Nov 30 2019
+// author: ctacampado
+
+// Package toylog implements a logging package based on
+// go's standard log library that features a configurable
+// logging level.
+package toylog
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+	"time"
+)
+
+// LogLvl is a custom type based on int.
+// It is used to differenciate a valid log level argument
+// vs some number
+type LogLvl int
+
+//logging levels
+const (
+	DEBUG LogLvl = iota
+	INFO
+	NOTICE
+	WARNING
+	ERR
+	CRIT
+	ALERT
+	EMRG
+)
+
+// A ToyLog represents a container object for a
+// logger and its current configuration parameters
+type ToyLog struct {
+	name     string      // name of the logger used for labeling logs for easier tracing
+	lvl      LogLvl      // logging level
+	File     *os.File    // stdout or file
+	FileName string      // log file name
+	logger   *log.Logger // pointer to the active logger object
+}
+
+// NewToyLog initializes ToyLog struct. Valid arguments are
+// as follows:
+// - type string for the logger name
+// - type int for the log level
+// - type *os.File for the output file
+// If it is an unknown parameter type, it returns nil
+func NewToyLog(args ...interface{}) (*ToyLog, error) {
+
+	tl := &ToyLog{
+		name: "logger",
+		lvl:  0,
+		File: os.Stdout,
+	}
+
+	for _, arg := range args {
+		switch a := arg.(type) {
+		case string:
+			if a != "" {
+				tl.name = fmt.Sprintf("[%s] ", a)
+			}
+		case LogLvl:
+			if a >= 0 && a <= EMRG {
+				tl.lvl = a
+			}
+		case bool:
+			if a == true {
+				fname := time.Now().Format("2006_01_02_15_04_05") + ".log"
+				f, err := os.OpenFile(fname,
+					os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+				if err != nil {
+					return nil, err
+				}
+				tl.File = f
+				tl.FileName = fname
+			}
+		default:
+			err := fmt.Sprintf("unknown parameter '%v' of type %s\n", arg, reflect.TypeOf(arg))
+			return nil, errors.New(err)
+		}
+	}
+
+	tl.logger = log.New(tl.File, tl.name, log.LstdFlags)
+	return tl, nil
+}
+
+// Close log file
+func (t *ToyLog) Close() {
+	t.File.Close()
+}
+
+// Info level is a logging level that uses log.LstdFlags only.
+// Using Info log level also shows Debug level logs
+func (t *ToyLog) Info(format string, v ...interface{}) {
+	if t.lvl >= INFO {
+		t.logger.SetFlags(log.LstdFlags)
+		newfmt := fmt.Sprintf("[INFO]: " + format)
+		t.logger.Output(2, fmt.Sprintf(newfmt, v...))
+	}
+}
+
+// Debug level is a logging level that uses log.LstdFlags | log.Lshortfile flags.
+// This is the default logging level
+func (t *ToyLog) Debug(format string, v ...interface{}) {
+	t.logger.SetFlags(log.LstdFlags | log.Lshortfile)
+	newfmt := fmt.Sprintf("[DEBUG]: " + format)
+	t.logger.Output(2, fmt.Sprintf(newfmt, v...))
+}
+
+// Error level is a logging level that uses log.LstdFlags | log.Lshortfile | log.Llongfile flags.
+// Using this log level will show all log levels less than or equal to ERR level
+func (t *ToyLog) Error(format string, v ...interface{}) {
+	t.logger.SetFlags(log.LstdFlags | log.Lshortfile | log.Llongfile)
+	if t.lvl >= ERR {
+		newfmt := fmt.Sprintf("[ERROR]: " + format)
+		t.logger.Output(2, fmt.Sprintf(newfmt, v...))
+	}
+}
---
+++ toylog_test.go
@@ -0,0 +1,112 @@
+package toylog
+
+import (
+	"log"
+	"os/exec"
+	"runtime"
+	"testing"
+)
+
+func TestNewToyLoggerNIL(t *testing.T) {
+	in := struct {
+		name string
+		lvl  LogLvl
+		file bool
+	}{
+		name: "logger",
+		lvl:  DEBUG,
+		file: false,
+	}
+
+	_, err := NewToyLog(in.name, in.lvl, 12345)
+	if nil != err {
+		t.Log(err.Error())
+	}
+}
+
+func TestNewToyLogToFile(t *testing.T) {
+	in := struct {
+		name string
+		lvl  LogLvl
+		file bool
+	}{
+		name: "logger",
+		lvl:  DEBUG,
+		file: true,
+	}
+
+	l, err := NewToyLog(in.name, in.lvl, in.file)
+	if nil != err {
+		t.Error(err.Error())
+	}
+
+	// delete created log file
+	if runtime.GOOS == "linux" {
+		cmd := exec.Command("rm", l.FileName)
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalf("cmd.Run() failed with %s\n", err)
+		}
+	}
+
+}
+
+func TestInfo(t *testing.T) {
+	in := struct {
+		name string
+		lvl  LogLvl
+		file bool
+		in   string
+	}{
+		name: "logger",
+		lvl:  INFO,
+		file: false,
+		in:   "hello world",
+	}
+	logger, err := NewToyLog(in.name, in.lvl, in.file)
+	if nil != err {
+		t.Error(err.Error())
+	}
+	logger.Info(in.in)
+	logger.Debug(in.in)
+}
+
+func TestDebug(t *testing.T) {
+	in := struct {
+		name string
+		lvl  LogLvl
+		file bool
+		in   string
+	}{
+		name: "logger",
+		lvl:  DEBUG,
+		file: false,
+		in:   "hello world",
+	}
+
+	logger, err := NewToyLog(in.name, in.lvl, in.file)
+	if nil != err {
+		t.Error(err.Error())
+	}
+	logger.Debug(in.in)
+}
+
+func TestError(t *testing.T) {
+	in := struct {
+		name string
+		lvl  LogLvl
+		file bool
+		in   string
+	}{
+		name: "logger",
+		lvl:  ERR,
+		file: false,
+		in:   "hello world",
+	}
+
+	logger, err := NewToyLog(in.name, in.lvl, in.file)
+	if nil != err {
+		t.Error(err.Error())
+	}
+	logger.Error(in.in)
+}
